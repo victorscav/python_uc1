b159b0d0080cea0cd82de86bcb5b59779ffed869
Victor Neves <vic_scav@hotmail.com>
2025-03-07 20:39:22 -0300

    Add files via upload

---
+++ aula03_tarefa01.py
@@ -0,0 +1,14 @@
+inteiro = 10
+ponto_flutuante = 3.14
+numero_complexo = 2 + 3j
+print(type(inteiro))
+print(type(ponto_flutuante))
+print(type(numero_complexo))
+nome = "Python"
+frase = 'Isso é uma string'
+multilinha = """Isso é uma string de múltiplas linhas!"""
+print(type(nome))
+verdadeiro = True
+falso = False
+print(type(verdadeiro))
+
