c1d844dc191c8020d1355646741d27831472e772
Victor Neves <vic_scav@hotmail.com>
2025-03-07 20:39:47 -0300

    Add files via upload

---
+++ aula03_tarefa02.py
@@ -0,0 +1,17 @@
+a = 10
+b = 3
+soma = a+b
+sub = a-b
+mult = a*b
+div = a/b
+divi = a//b
+modulo = a%b
+exp = a**b
+
+print(f"O resultado da soma é {soma}")
+print(f"O resultado da subtração é {sub}")
+print(f"O resultado da multiplicação é {mult}")
+print(f"O resultado da divisão é {div}")
+print(f"O resultado da divisão real é {divi}")
+print(f"O resultado do módulo é {modulo}")
+print(f"O resultado da exponenciação é {exp}")
---
+++ aula03_tarefa03.py
@@ -0,0 +1,18 @@
+a=float(input("Digite o 1º número : "))
+b=float(input("Digite o 2º número : "))
+
+soma=a+b
+sub = a-b
+mult = a*b
+div = a/b
+divi = a//b
+modulo = a%b
+exp = a**b
+
+print(f"O resultado da soma é {soma}")
+print(f"O resultado da subtração é {sub}")
+print(f"O resultado da multiplicação é {mult}")
+print(f"O resultado da divisão é {div:.2f}")
+print(f"O resultado da divisão real é {divi}")
+print(f"O resultado do módulo é {modulo}")
+print(f"O resultado da exponenciação é {exp}")
