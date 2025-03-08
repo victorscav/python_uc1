23801fc1a56a1086d0dc0e523b32d5bb499c7a73
Victor Neves <vic_scav@hotmail.com>
2025-03-07 21:45:00 -0300

    Add files via upload

---
+++ aula03_tarefa04.py
@@ -0,0 +1,32 @@
+x=10
+
+print(f"O valor de x é : {x}")
+
+x+=5
+
+print(f"O valor de x é : {x}")
+
+x-=3
+
+print(f"O valor de x é : {x}")
+
+x*=2
+
+print (f"O valor de x é : {x}")
+
+x/=4
+
+print (f"O valor de x é : {x}")
+
+x//=3
+
+print (f"O valor de x é {x}")
+
+x%=2
+
+print (f"O valor de x é {x}")
+
+x**=3
+
+print (f"O valor de x é : {x}")
+
