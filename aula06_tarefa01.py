e77c7b970a889543df9f3f0c38404d4a55c1e448
Victor Neves <vic_scav@hotmail.com>
2025-03-14 21:16:07 -0300

    Add files via upload

---
+++ aula06_tarefa01.py
@@ -0,0 +1,16 @@
+frutas = ["maçã", "banana", "cereja"]
+
+for item in frutas:
+    print(f"\t {item.upper()}")
+    input("\n Pressione ENTER \n")
+
+
+contagem = (9,8,7,6,5,4,3,2,1,0)
+for item in contagem:
+    print(f"{item}...")
+
+for i in range(1000):
+    print(i)
+
+for i in range(3):
+    print (f"{i} - {frutas[i]}")
