983f501740fdc6d4e5f1f3aace7586bdd7b5b05d
Victor Neves <vic_scav@hotmail.com>
2025-03-19 18:43:22 -0300

    Add files via upload

---
+++ aula06_desafio06.5.py
@@ -0,0 +1,10 @@
+while True:
+    nome = input("Digite seu nome, ou digite sair para cancelar: ")
+
+    if nome == "sair":
+        print("Programa Cancelado!")
+        break
+
+    else:
+        print (f"Bem vindo {nome}!")
+    
